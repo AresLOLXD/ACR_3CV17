fc775b48452e5f906059b54a704f72932ba41182
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2022-03-24 20:24:15 -0600

    Terminada la tarea

--- Tareas/T5/Cliente.java
+++ Tareas/T5/Cliente.java
@@ -10,16 +10,20 @@ public class Cliente {
             String host = br1.readLine();
             System.out.printf("\n\nEscriba el puerto: ");
             int pto = Integer.parseInt(br1.readLine());
-            Socket cl = new Socket(host, pto);
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getInputStream()));
-            String mensaje = br2.readLine();
+            Socket cl = new Socket(host, pto); // Creamos un socket cliente, apuntando al servidor y al puerto para la
+                                               // conexion
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getInputStream())); // Creamos un stream
+                                                                                                 // para el comunicacion
+                                                                                                 // entre el servidor
+                                                                                                 // cliente
+            String mensaje = br2.readLine(); // Leemos el mensaje del stream
             System.out.println("Recibimos un mensaje desde el servidor");
             System.out.println("Mensaje: " + mensaje);
-            br1.close();
-            br2.close();
-            cl.close();
+            br1.close(); // Cerramos la conexion al stream de la consola
+            br2.close();// Cerramos la conexion al stream del socket
+            cl.close(); // Desconectamos el socket
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Por si acaso
         }
     }
 }
--- Tareas/T5/SHola.java
+++ Tareas/T5/SHola.java
@@ -4,17 +4,19 @@ import java.io.*; //Importamos las bibliotecas para los streams(flujos)
 public class SHola {
     public static void main(String args[]) {
         try {
-            ServerSocket s = new ServerSocket(1234);
+            ServerSocket s = new ServerSocket(1234); // Iniciamos un socket servidor en el puerto 1234
             System.out.println("Esperando cliente ...");
             for (;;) {
-                Socket cl = s.accept();
+                Socket cl = s.accept(); // Esperamos a la conexion de un socket cliente para la comunicacion
                 System.out.println("Conexión establecida desde " + cl.getInetAddress() + ":" + cl.getPort());
                 String mensaje = "Ares Ulises Juárez Martínez";
-                PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
-                pw.println(mensaje);
-                pw.flush();
-                pw.close();
-                cl.close();
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream())); // Creamos un stream
+                                                                                                // para escribir en el
+                                                                                                // socket
+                pw.println(mensaje); // Escribimos el mensaje
+                pw.flush(); // Enviamos la informacion
+                pw.close(); // Cerramos el stream
+                cl.close(); // Cerramos el socket
             } // for
         } catch (Exception e) {
             e.printStackTrace();
